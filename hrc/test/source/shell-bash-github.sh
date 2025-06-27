297f3886ea1b2400f6ad0440481d2b2c9a0cc680
Konstantin Kushnir <chpock@gmail.com>
2025-06-27 16:57:18 +0000

    bash: arithmetic evaluation is not recognized inside a for loop
    Closes #12

--- hrc/test/source/shell-bash-github.sh
+++ hrc/test/source/shell-bash-github.sh
@@ -19,3 +19,14 @@ ids+=( $! )
 # https://github.com/colorer/Colorer-schemes/issues/172
 
 echo "${!var@Q}" more text here
+
+# https://github.com/chpock/ck.colorer-schemes/issues/12
+
+(( A + 1 ))
+for i; do
+    (( A + 1 ))
+done
+
+for (( i = 0; i < L; i++ )); do
+    (( B = (B + A) % 65521 )) || :
+done
